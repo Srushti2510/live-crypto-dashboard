974e6f41f7486e8e8d4bea17deae035e4deddac3
Srushti2510 <srushtikurhade2510@gmail.com>
2026-07-05 23:54:56 +0530

    Day 2: Added largest of three and leap year challenges

---
+++ Day02/logic_challenge/largest_of_three.js
@@ -0,0 +1,13 @@
+let a = 15;
+let b = 27;
+let c = 19;
+
+if (a > b && a > c) {
+    console.log(a, "is largest");
+}
+else if (b > a && b > c) {
+    console.log(b, "is largest");
+}
+else {
+    console.log(c, "is largest");
+}
