968b41aae5441fcddc69b5db085bbf8970c3bf7c
Srushti2510 <srushtikurhade2510@gmail.com>
2026-07-19 22:56:07 +0530

    feat: add first javascript variables test

---
+++ basics.js
@@ -0,0 +1,14 @@
+// 1. Declaring variables (No need to specify int, float, or string!)
+let programmingLanguage = "JavaScript"; 
+const targetMonth = "October"; 
+let daysRemaining = 90;
+
+// 2. Printing to the console (This is JS's version of cout or printf)
+console.log("--- My Internship Mission ---");
+console.log("Language to master: " + programmingLanguage);
+console.log("Target deadline: " + targetMonth);
+console.log("Days left: " + daysRemaining);
+
+// 3. Changing a variable (let can change, const cannot)
+daysRemaining = 89; 
+console.log("Update: Now " + daysRemaining + " days left!");
