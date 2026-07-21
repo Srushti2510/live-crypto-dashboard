1d16819b4421faa1c1ab5e8b7a4dedb12ef8e204
Srushti2510 <srushtikurhade2510@gmail.com>
2026-07-21 23:21:39 +0530

    feat: added if/else conditional logic

---
+++ if_else.js
@@ -0,0 +1,8 @@
+let studentScore = 85;
+let cutoffScore = 80;
+
+if (studentScore >= cutoffScore) {
+    console.log("Status: Selected for the MERN Internship!");
+} else {
+    console.log("Status: Keep grinding, try next round.");
+}
