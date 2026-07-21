e0b4869081f6275094d1b25cba127b0a085fde4f
Srushti2510 <srushtikurhade2510@gmail.com>
2026-07-21 23:24:34 +0530

    feat: combined arrays with for-loops for automation

--- arrays.js
+++ arrays.js
@@ -10,3 +10,9 @@ console.log(`My frontend framework will be: ${techStack[2]}`);
 // 3. Add a new skill to the end of the list using .push()
 techStack.push("TailwindCSS");
 console.log(`Updated Stack: ${techStack}`);
+
+
+// The Loop: Start at 0, keep going as long as we haven't hit the end of the array, and go up by 1 each time.
+for (let i = 0; i < techStack.length; i++) {
+    console.log(`Skill ${i + 1}: ${techStack[i]}`);
+}
